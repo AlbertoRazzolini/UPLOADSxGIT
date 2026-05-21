8cf76cc22cc86c118de19ee7f18478b95d7e992b
AlbertoRazzolini <alrazzo2@libero.it>
2026-05-21 13:31:43 +0200

    Live coding 1

--- Live coding/assets/js/script.js
+++ Live coding/assets/js/script.js
@@ -36,10 +36,41 @@ const printCurrentMonth = () => {
 
 printCurrentMonth();
 
-const dayInMonth = () => {
+const daysInMonth = () => {
   const lastDay = new Date(getYear, getMonth + 1, 0); // gli stiamo chiedendo il giorno 0 del mese successivo, ad esempio lo 0 giugno 2026, che sarà il 31 Maggio
   const numberOfDays = lastDay.getDate();
   return numberOfDays;
 };
 
-dayInMonth();
+daysInMonth();
+
+// Creiamo la griglia
+const createDays = (daysNumber) => {
+  const calendarDiv = document.querySelector("#calendar");
+  for (let i = 1; i <= daysNumber; i++) {
+    const dayCellDiv = document.createElement("div");
+    dayCellDiv.classList.add("day");
+    // le celle dovranno essere cliccabili - DA FARE
+
+    // creiamo il giorno
+    const cellValue = document.createElement("h3");
+    const thisDate = i;
+    // evidenziamo il giorno corrente
+    if (thisDate === now.getDate()) {
+      dayCellDiv.classList.add("currentDay");
+    }
+    // scriviamo le domeniche in rosso
+    let thisDay = new Date(getYear, getMonth, thisDate);
+    if (thisDay.getDay() === 0) {
+      cellValue.classList.add("sunday");
+    }
+    // scriviamo il nome del giorno
+    let dayNumber = thisDay.getDay();
+    let dayName = dayNames[dayNumber];
+    cellValue.textContent = `${dayName} ${i}`;
+    dayCellDiv.appendChild(cellValue);
+    calendarDiv.appendChild(dayCellDiv);
+  }
+};
+
+createDays(daysInMonth());
